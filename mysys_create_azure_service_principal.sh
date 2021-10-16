96b622be4d8ef06a9c4e3bc5de1d3a5d92d2c6b3
joao tiago viegas <jtviegas.js@gmail.com>
2021-10-16 23:36:50 +0200

    refactoring

--- mysys_create_azure_service_principal.sh
+++ mysys_create_azure_service_principal.sh
@@ -1,82 +1,94 @@
 #!/usr/bin/env bash
 
-# ===> COMMON SECTION START  ===>
-# http://bash.cumulonim.biz/NullGlob.html
-shopt -s nullglob
-
-# ---------- CONSTANTS ----------
-export USER_BIN_DIR=~/.local/bin
-export INCLUDES_DIR=~
-# -------------------------------
-
 if [ -z "$this_folder" ]; then
   this_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
   if [ -z "$this_folder" ]; then
     this_folder=$(dirname $(readlink -f $0))
   fi
 fi
-if [ -z "$parent_folder" ]; then
-  parent_folder=$(dirname "$this_folder")
-fi
 
-debug(){
-    local __msg="$@"
-    echo " [DEBUG] `date` ... $__msg "
+. "$this_folder/common.inc"
+
+on_no_assign()
+{
+  info "[on_no_assign|in]"
+  sp_name="$1"
+  az ad sp create-for-rbac -n "$sp_name" --skip-assignment
+  info "[on_no_assign|out]"
 }
 
-info(){
-    local __msg="$@"
-    echo " [INFO]  `date` ->>> $__msg "
+az_sp_on()
+{
+  info "[az_sp_on|in]"
+  az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/${ARM_SUBSCRIPTION_ID}" -o table
+  info "[az_sp_on] please add the following output to '.secrets' file:     password(ARM_CLIENT_SECRET) "
+  info "[az_sp_on] please add the following output to '.variables' file:   app_id(ARM_CLIENT_ID), tenant(ARM_TENANT_ID)"
+  info "[az_sp_on|out]"
 }
 
-warn(){
-    local __msg="$@"
-    echo " [WARN]  `date` *** $__msg "
+az_sp_off()
+{
+  info "[az_sp_off|in]"
+  az ad sp delete --id "${ARM_CLIENT_ID}"
+  info "[az_sp_off|out]"
 }
 
-err(){
-    local __msg="$@"
-    echo " [ERR]   `date` !!! $__msg "
+az_sp_login()
+{
+  info "[az_sp_login|in]"
+  az login --service-principal -u "${ARM_CLIENT_ID}" -p "${ARM_CLIENT_SECRET}" --tenant "${ARM_TENANT_ID}"
+  info "[az_sp_login|out]"
 }
 
-verify_prereqs(){
-  info "[verify_prereqs] ..."
-  for arg in "$@"
-  do
-      debug "[verify_prereqs] ... checking $arg"
-      which "$arg" 1>/dev/null
-      if [ ! "$?" -eq "0" ] ; then err "[verify_prereqs] please install $arg" && return 1; fi
-  done
-  info "[verify_prereqs] ...done."
+az_sp_check()
+{
+  info "[az_sp_check|in]"
+  az vm list-sizes --location westus
+  info "[az_sp_check|out]"
 }
 
-if [ ! -f "$INCLUDES_DIR/.variables.inc" ]; then
-  debug "we DON'T have a '$INCLUDES_DIR/.variables.inc' file"
-else
-  . "$INCLUDES_DIR/.variables.inc"
-fi
-
-if [ ! -f "$INCLUDES_DIR/.secrets.inc" ]; then
-  debug "we DON'T have a '$INCLUDES_DIR/.secrets.inc' file"
-else
-  . "$INCLUDES_DIR/.secrets.inc"
-fi
-
-# <=== COMMON SECTION END  <===
-
-# parameter check
 usage()
 {
-        cat <<EOM
-        usage:
-        $(basename $0) <service_principal_name>
-            creates an azure service principal with no assignment
+  cat <<EOM
+  usages:
+  $(basename $0) {on|off|login|check|on_no_assign}
+                          on
+                            creates azure service principal with contributor role and the current account subscription
+                          off
+                            deletes the service principal with id defined in env var ARM_CLIENT_ID
+                          login
+                            logs in using the service principal credentials defined in environment
+                          check
+                            checks if logged in correctly listing VM's sizes
+                          on_no_assign  <sp name>
+                            creates azure service principal with no assignment
 EOM
-        exit 1
+  exit 1
 }
 
-[ -z "$1" ] && { usage; }
+info "starting [ $0 $1 $2 ] ..."
+_pwd=$(pwd)
 
-sp_name="$1"
+case "$1" in
+      on)
+        az_sp_on
+        ;;
+      off)
+        az_sp_off
+        ;;
+      login)
+        az_sp_login
+        ;;
+      check)
+        az_sp_check
+        ;;
+      on_no_assign)
+        [ -z "$2" ] && err "[on_no_assign] <sp name> param is missing" && usage
+        on_no_assign "$2"
+        ;;
+      *)
+        usage
+        ;;
+esac
 
-az ad sp create-for-rbac -n "$sp_name" --skip-assignment
+info "...[ $0 $1 $2 ] done."
--- mysys_update.sh
+++ mysys_update.sh
@@ -1,68 +1,13 @@
 #!/usr/bin/env bash
 
-# ===> COMMON SECTION START  ===>
-# http://bash.cumulonim.biz/NullGlob.html
-shopt -s nullglob
-
-# ---------- CONSTANTS ----------
-export USER_BIN_DIR=~/.local/bin
-export INCLUDES_DIR=~
-# -------------------------------
-
 if [ -z "$this_folder" ]; then
   this_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
   if [ -z "$this_folder" ]; then
     this_folder=$(dirname $(readlink -f $0))
   fi
 fi
-if [ -z "$parent_folder" ]; then
-  parent_folder=$(dirname "$this_folder")
-fi
 
-debug(){
-    local __msg="$@"
-    echo " [DEBUG] `date` ... $__msg "
-}
-
-info(){
-    local __msg="$@"
-    echo " [INFO]  `date` ->>> $__msg "
-}
-
-warn(){
-    local __msg="$@"
-    echo " [WARN]  `date` *** $__msg "
-}
-
-err(){
-    local __msg="$@"
-    echo " [ERR]   `date` !!! $__msg "
-}
-
-verify_prereqs(){
-  info "[verify_prereqs] ..."
-  for arg in "$@"
-  do
-      debug "[verify_prereqs] ... checking $arg"
-      which "$arg" 1>/dev/null
-      if [ ! "$?" -eq "0" ] ; then err "[verify_prereqs] please install $arg" && return 1; fi
-  done
-  info "[verify_prereqs] ...done."
-}
-
-if [ ! -f "$INCLUDES_DIR/.variables.inc" ]; then
-  debug "we DON'T have a '$INCLUDES_DIR/.variables.inc' file"
-else
-  . "$INCLUDES_DIR/.variables.inc"
-fi
-
-if [ ! -f "$INCLUDES_DIR/.secrets.inc" ]; then
-  debug "we DON'T have a '$INCLUDES_DIR/.secrets.inc' file"
-else
-  . "$INCLUDES_DIR/.secrets.inc"
-fi
-
-# <=== COMMON SECTION END  <===
+. "$this_folder/common.inc"
 
 if [ ! -d "$USER_BIN_DIR" ]; then
   err "can't find local bin folder: $USER_BIN_DIR"
@@ -72,9 +17,11 @@ fi
 _pwd=$(pwd)
 
 cd "$USER_BIN_DIR"
+rm mysys_*
 curl -s https://api.github.com/repos/tgedr/mysys-bash-scripts/releases/latest \
 | grep "browser_download_url.*mysys\.tar\.bz2" \
 | cut -d '"' -f 4 | wget -qi -
 tar xjpvf mysys.tar.bz2
 rm mysys.tar.bz2
+
 cd "$_pwd"
